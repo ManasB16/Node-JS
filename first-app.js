ca68f1704193c004b8e4af75e745375483413dd1
ManasB16 <manasbhola@gmail.com>
2023-06-15 23:01:24 +0530

    created first server

--- first-app.js
+++ first-app.js
@@ -1,3 +1,12 @@
-console.log('Hello World!');
+// console.log('Hello World!');
+
 // const fs = require('fs')
 // fs.writeFileSync('hello.txt', 'hello world!')
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log('Manas')
+})
+
+server.listen(4000)
