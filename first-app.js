3bedc17ad6e4d4aefe2e76693b90a5d8053aeb87
ManasB16 <manasbhola@gmail.com>
2023-06-16 12:06:27 +0530

    First Node Page

--- first-app.js
+++ first-app.js
@@ -6,7 +6,28 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log('Manas')
+    const url = req.url
+    if (url === '/home') {
+        res.write('<html>')
+        res.write('<head><title>Home</title></head>')
+        res.write("<body><h1>Welcome home</h1></body>");
+        res.write('</html>')
+        res.end();
+    }
+    if (url === "/about") {
+      res.write("<html>");
+      res.write("<head><title>About Us</title></head>");
+      res.write("<body><h1>Welcome to About Us page</h1></body>");
+      res.write("</html>");
+      res.end();
+    }
+    if (url === "/node") {
+      res.write("<html>");
+      res.write("<head><title>Node JS</title></head>");
+      res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+      res.write("</html>");
+      res.end();
+    }
 })
 
 server.listen(4000)
