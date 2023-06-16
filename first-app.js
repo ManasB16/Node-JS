7cbe87ab4bec6dce636b50358cdd741d65beaf23
ManasB16 <93585483+ManasB16@users.noreply.github.com>
2023-06-16 12:22:50 +0530

    first-app.js

--- first-app.js
+++ first-app.js
@@ -12,21 +12,21 @@ const server = http.createServer((req, res) => {
         res.write('<head><title>Home</title></head>')
         res.write("<body><h1>Welcome home</h1></body>");
         res.write('</html>')
-        res.end();
+        return res.end();
     }
     if (url === "/about") {
       res.write("<html>");
       res.write("<head><title>About Us</title></head>");
       res.write("<body><h1>Welcome to About Us page</h1></body>");
       res.write("</html>");
-      res.end();
+      return res.end();
     }
     if (url === "/node") {
       res.write("<html>");
       res.write("<head><title>Node JS</title></head>");
       res.write("<body><h1>Welcome to my Node Js project</h1></body>");
       res.write("</html>");
-      res.end();
+      return res.end();
     }
 })
 
