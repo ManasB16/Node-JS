a773f46a0601a151cf4e07dacd896ac05dcae28b
ManasB16 <93585483+ManasB16@users.noreply.github.com>
2023-06-17 02:49:29 +0530

    routes.js

--- routes.js
+++ routes.js
@@ -40,3 +40,13 @@ const requestHandler = (req, res) => {
 };
 
 module.exports = requestHandler;
+
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'HI'
+// }
+
+// module.exports.handler = requestHandler
+// module.exports.someText = 'HI'
+
+// exports.handler = requestHandler
