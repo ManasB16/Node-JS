213b196b1ba5ceb2565d9e51569f1a269ba16ab3
ManasB16 <manasbhola@gmail.com>
2023-06-18 17:43:57 +0530

    Starting Express

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('1st middleware')
-    next();     // Allows the request to continue to the next middleware in line.
-})
+  console.log("1st middleware");
+  next(); // Allows the request to continue to the next middleware in line.
+});
 
 app.use((req, res, next) => {
   console.log("2nd middleware");
   res.send(`<h1>Hello to Express.js</h1>`);
 });
 
-app.listen(4000)
+app.listen(4000);
