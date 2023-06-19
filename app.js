627a0a3693b56202716375c1f7ba39253587f9ef
ManasB16 <manasbhola@gmail.com>
2023-06-19 15:49:27 +0530

    routers and filters

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-// const http = require("http");
-
 const express = require("express");
-
 const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use("/admin", adminRoute);
+app.use("/shop", shopRoute);
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send(`<h1>Product added</h1>`);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Error 404");
 });
 
 app.listen(4000);
